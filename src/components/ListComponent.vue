50150e882513d041023a2e922ea9af6b85d2c274
atyha <atyhaarshad1@hotmail.com>
2019-05-02 09:08:44 +0100

    mvp

---
+++ src/components/ListComponent.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div class="">
+  <!-- <li v-on:click="handleClick">{{country.name}}</li> -->
+
+  <select v-on:click="handleClick">
+    <option v-for="(for country in countries)">{{country.name}}</option>
+  </select>
+  </div>
+
+</template>
+
+<script>
+import {eventBus} from '../main.js'
+
+export default {
+  name: 'list-component',
+  props: ['country'],
+  methods: {
+    handleClick: function(){
+      eventBus.$emit('country-selected', this.country)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/countriesList.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <ul>
+    <list-component v-for="(country, index) in countries" :country="country" :key="index"></list-component>
+  </ul>
+</template>
+
+<script>
+import ListComponent from './ListComponent.vue';
+
+export default {
+  name: 'countries-list',
+  props: ['countries'],
+  components: {
+    "list-component": ListComponent
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
